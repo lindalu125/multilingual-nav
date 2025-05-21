506b6f7997a7974ba290a0618a78dba2ed06ec28
lindalu125 <61084945+lindalu125@users.noreply.github.com>
2025-05-22 00:08:43 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+
+const interSans = Inter({
+  variable: '--font-inter-sans',
   subsets: ['latin']
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-})
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${interSans.variable} antialiased`}>{children}</body>
     </html>
   )
 }
